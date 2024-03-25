4c0e10ddea2f2507369772ae6e03a799381eee65
Arjun Aditya <73933669+nermalcat69@users.noreply.github.com>
2024-03-25 23:57:41 +0530

    feat: navbar component

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Card } from "@repo/ui/card";
+import NavBar from "@repo/ui/navbar"
 
 function Gradient({
   conic,
@@ -46,22 +47,7 @@ const LINKS = [
 export default function Page(): JSX.Element {
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
-      <div className="z-10 items-center justify-between w-full max-w-7xl font-mono text-md lg:flex">
-        <h2 className="fixed top-0 left-0 flex justify-center w-full px-4 pt-8 pb-6 border-b bg-gradient-to-b backdrop-blur-2xl border-neutral-800 bg-zinc-800/30 from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:bg-zinc-800/30">
-          r/JEENEETards
-        </h2>
-        <div className="fixed bottom-0 left-0 flex items-end justify-center w-full h-48 bg-gradient-to-t from-black via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="flex gap-2 p-8 pointer-events-none place-items-center lg:pointer-events-auto lg:p-0"
-            href="https://reddit.com/r/JEENEETards"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            <p>Reddit</p> 
-          </a>
-        </div>
-      </div>
-
+    <NavBar />
       <div className="relative flex place-items-center ">
         <div className="font-sans w-auto pb-16 pt-[48px] md:pb-24 lg:pb-32 md:pt-16 lg:pt-20 flex justify-between gap-8 items-center flex-col relative z-0">
           <div className="z-50 flex items-center justify-center w-full">
--- packages/ui/src/header.tsx
+++ packages/ui/src/header.tsx
@@ -1,7 +1,19 @@
 export default function NavBar() {
     return (
-        <div>
-            d
-    </div>
+      <div className="z-10 items-center justify-between w-full max-w-7xl font-mono text-md lg:flex">
+        <h2 className="fixed top-0 left-0 flex justify-center w-full px-4 pt-8 pb-6 border-b bg-gradient-to-b backdrop-blur-2xl border-neutral-800 bg-zinc-800/30 from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:bg-zinc-800/30">
+          r/JEENEETards
+        </h2>
+        <div className="fixed bottom-0 left-0 flex items-end justify-center w-full h-48 bg-gradient-to-t from-black via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+          <a
+            className="flex gap-2 p-8 pointer-events-none place-items-center lg:pointer-events-auto lg:p-0"
+            href="https://reddit.com/r/JEENEETards"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <p>Reddit</p> 
+          </a>
+        </div>
+      </div>    
 )
 }
